23acf9e5a697afaa121c2d25479e18c9f48f3997
Tom Oeste <tom.oeste@bullseye-computing.com>
2020-09-06 17:41:10 -0400

    Replaced failing boilerplate test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders theme toggle button', () => {
+  const { getByLabelText } = render(<App />);
+  const buttonElement = getByLabelText('Toggle theme');
+  expect(buttonElement).toBeInTheDocument();
 });
+
+test('renders skills heading', () => {
+  const { getByText } = render(<App />);
+  const headingElement = getByText('Skills');
+  expect(headingElement).toBeInTheDocument();
+});
+
+test('renders expertise heading', () => {
+  const { getByText } = render(<App />);
+  const headingElement = getByText('Expertise');
+  expect(headingElement).toBeInTheDocument();
+});
+
